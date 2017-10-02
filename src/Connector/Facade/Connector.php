73bd5dcebce2ca9ed6ecc36f66f0dde25ea0cae0
nzolt <nzolt@dotweb.hu>
2017-10-02 08:09:47 +0100

    Add Docblock header

--- src/Connector/Facade/Connector.php
+++ src/Connector/Facade/Connector.php
@@ -1,9 +1,22 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: nzolt
- * Date: 30/09/2017
- * Time: 02:10
+ * Composer package
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@magentocommerce.com so we can send you a copy immediately.
+ *
+ * @category   Time2
+ * @package    Time2_BookList
+ * @author     Time2 Digital Limited <zoltan.nagy@time2.digital>
+ * @copyright  Copyright (c) 2017 Time2 Digital Limited (http://www.time2.digital)
+ * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
 
 namespace BookList\Connector\Facade;
